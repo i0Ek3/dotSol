8004a98051abb91bdbd0e47012c7c1cc33dcc7d3
i0Ek3 <kno30826@gmail.com>
2018-10-16 12:46:40 +0800

    Add src

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # dotSol
-Smart contract code snippets.
+
+> Smart contract code snippets.
+
+Here are some smart contract code snippets, and I want to understand how smart contract to work, just for fun.
+
+No words for code, take it by yourself.
+
+
+## Code
+
+- Ballot.sol
+-
+-
+-
+
+
+
+## Compile
+
+More compile detail informations you can visit [here](https://solidity.readthedocs.io/en/v0.4.25/using-the-compiler.html).
+
+```Shell
+$ npm install solc
+$ solc targetfile
+
+```
+
+
+## Ref
+
+- [solidity](https://github.com/ethereum/solidity) Ethereum official language for smart contract.
+- [solidity-doc](https://solidity.readthedocs.io/en/v0.4.24/index.html) Official solidity doc.
+- [solc](https://github.com/ethereum/solc-js) Solidity compiler.
+- [remix-ide](https://github.com/ethereum/remix-ide) Smart contract IDE.
---
+++ src/ballot.sol
@@ -0,0 +1,92 @@
+pragma solidity ^0.4.24;
+
+contract Ballot { // voting with delegation
+
+        struct Voter {
+                uint weight; // for delegation
+                bool voted; // true = voted
+                address delegate;
+                uint vote; // index of voted proposal
+        }
+
+        struct Proposal {
+                bytes32 name;
+                uint voteCount;
+        }
+
+        address public chairperson;
+        mapping(address => Voter) public voters; // type: mapping
+        Proposal[] public proposals;
+        
+        constructor(bytes32[] proposalNames) public { // a new ballot to choose one of proposalNames
+                chairperson = msg.sender;
+                voters[chairperson].weight = 1;
+                for (uint i = 0; i < proposalNames.length; i++) { // create a proposal object to store proposalNames and it's vote count
+                        proposals.push(Proposal({
+                                name: proposalNames[i],
+                                voteCount: 0
+                        }));
+                }
+        
+        }
+        
+        function grantVote(address voter) public {
+                require(
+                        msg.sender == chairperson,
+                        "Only chairperson can give right to vote."
+                );
+                require(
+                        !voters[voter].voted,
+                        "The voter already voted."
+                );
+                require(voters[voter].weight == 0);
+                voters[voter].weight == 1;
+        }
+        
+        function delegate(address to) public {
+                Voter storage sender = voters[msg.sender];
+                require(!sender.voted, "You already voted.");
+                require(to != msg.sender, "Self-delegation is disallowed.");
+                while (voters[to].delegate != address(0)) {
+                        to = voters[to].delegate;
+                        require(to != msg.sender, "Found loop in delegation.");
+                }
+                sender.voted = true;
+                sender.delegate = to;
+                Voter storage delegate_ = voters[to];
+                if (delegate_.voted) {
+                        proposal[delegate_.vote].voteCount += sender.weight;
+                } else {
+                        delegate_.weight += sender.weight;
+                }
+        }
+        
+        function vote(uint proposal) public {
+                Voter storage sender = voters[msg.sender];
+                require(!sender.voted, "Already voted.");
+                sender.voted = true;
+                sender.vote = proposal;
+        }
+        
+        function winProposal() public view
+                returns (uint winProposal_)
+        {
+                uint winVoteCount = 0;
+                for (uint p = 0; p < proposals.length; p++) {
+                        if (proposals[p].voteCount > winVoteCount) {
+                                winVoteCount = proposals[p].voteCount;
+                                winProposal_ = p;
+                        }
+                }
+        }
+        
+        function winnerName() public view
+                returns (bytes32 winnerName_)
+        {
+                winName_ = proposals[winProposal()].name;
+        }
+}
+
+
+
+
